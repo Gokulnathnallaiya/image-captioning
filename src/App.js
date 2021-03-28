4ed8f0f2a48b046f124ddfe112fe33ec1f1c4942
Gokulnath <gokulaps1998@gmail.com>
2021-03-28 13:22:19 +0530

    voice command added

--- src/App.js
+++ src/App.js
@@ -2,42 +2,68 @@ import axios from "axios";
 import React from "react";
 import ImageUploader from "react-images-upload";
 import "./App.css";
+import Speech from "react-speech";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { pictures: [], caption: "", loading:false };
+    this.state = { pictures: [], caption: null, loading: false};
     this.onDrop = this.onDrop.bind(this);
-    
   }
 
   onDrop(picture) {
+    this.setState({ loading: true });
 
-    this.setState({loading:true})
-    
-    axios.post("http://bee09e52ec85.ngrok.io/predict", {userImage:picture[0].name}).then(res=>{
-      console.log(res)
-      this.setState({caption:res.data.caption, loading:false})
-      
-    });
+    axios
+      .post("http://bee09e52ec85.ngrok.io/predict", {
+        userImage: picture[0].name,
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({ caption: res.data.caption, loading: false });
+      });
   }
 
   render() {
     return (
       <div className="upload-container">
         <h1>IMAGE-CAPTIONING</h1>
-<div style={{maxWidth:'600px'}}>
-        <ImageUploader
-          withIcon={true}
-          buttonText="Choose images"
-          onChange={this.onDrop}
-          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-          maxFileSize={5242880}
-          withPreview={true}
-        />
-</div>
+        <div style={{ maxWidth: "600px" }}>
+          <ImageUploader
+            withIcon={true}
+            buttonText="Choose images"
+            onChange={this.onDrop}
+            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            maxFileSize={5242880}
+            withPreview={true}
+          />
+        </div>
+        
 
-        {this.state.loading ?<p>Please wait while we caption Your image 🙃</p>:<p>Caption : {this.state.caption}</p>}
+        {this.state.loading ? (
+          <p className="loading">
+            Sit Back and relax! while we find the right caption for your image
+            🙃
+          </p>
+        ) : ( this.state.caption ?
+          <>
+            <p>
+              Caption :
+              <span style={{ color: "green" }}>"{this.state.caption}"</span>
+            </p>
+            <div>
+              <Speech
+                text={this.state.caption}
+                
+                stop={true}
+                
+                volume={10}
+              />
+            </div>
+          </>:(
+            <p>Upload an image to find the caption</p>
+          )
+        )}
       </div>
     );
   }
